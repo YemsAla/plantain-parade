402935625b0a15c207583c8d5e2a1e910886a4ae
Yemi Alade <ylawal7@gmail.com>
2026-06-16 14:26:45 +0100

    Connect Stripe payment intent to checkout view
    - Install stripe package
    - Create payment intent in checkout view using bag total
    - Pass real client secret to template
    - Add STRIPE_CURRENCY setting
    - Add stripe card error listener in stripe_elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
 
+import stripe
+
+from bag.contexts import bag_contents
 from .forms import OrderForm
 
 def checkout(request):
@@ -13,13 +16,22 @@ def checkout(request):
     if not bag:
         messages.error(request, "There's nothing in your bag right now")
         return redirect(reverse('products'))
+
+    current_bag = bag_contents(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
     
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': 'test_client_secret',
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
