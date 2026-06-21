2f736d6e5fd53272862c9be803426a7958f21946
Yemi Alade <ylawal7@gmail.com>
2026-06-22 00:29:20 +0100

    Add profile support to webhook handler with save info logic

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,6 +3,9 @@ import stripe
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+
+from django.contrib.auth.models import User
 
 import json
 import time
@@ -21,7 +24,7 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
-    
+
     def handle_payment_intent_succeeded(self, event):
         """
         Handle the payment_intent.succeeded webhook from Stripe
@@ -44,6 +47,22 @@ class StripeWH_Handler:
         for field in ['city', 'country', 'line1', 'line2', 'postal_code', 'state']:
             if address[field] == "":
                 address[field] = None
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            user = User.objects.get(username=username)
+            profile, created = UserProfile.objects.get_or_create(user=user)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -77,6 +96,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -113,11 +133,11 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-        
+
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
-    
+
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
